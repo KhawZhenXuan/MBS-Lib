7eb4303381bd02a55d7ba144fc30e052d0771eba
KhawZhenXuan <147393678+KhawZhenXuan@users.noreply.github.com>
2024-10-09 23:26:15 +0800

    Delete ME MDM-Laptop Enrollment directory

--- ME MDM-Laptop Enrollment/enrollment.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-rem ------------------------------------
-rem Calling scripts\enrollment.bat as Administrator
-rem ------------------------------------
-set CUR_DIR=%~dp0
-"%CUR_DIR%scripts\RunAsAdmin.exe" "%CUR_DIR%scripts\enrollmentIntermediate.bat"
-
-exit
--- ME MDM-Laptop Enrollment/scripts/enrollmentIntermediate.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-rem -----------------------------
-rem This intermediate file is for 
-rem allowing enrollment.bat to print 
-rem output lines in command prompt
-rem -----------------------------
-
-call start "MDM Enrollment" "%CUR_DIR%scripts\enrollment.bat"
